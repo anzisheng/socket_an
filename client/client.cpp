66141337de510ee4b3485432b1bdc8181941a38e
anzisheng <anzisheng@163.com>
2025-02-07 16:11:16 +0800

    client is ok

---
+++ client/client.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+int main(){
+    int sock;
+    struct sockaddr_in server;
+
+    //create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        std::cerr << "socket creation failed" <<std::endl;
+        return 1;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8080);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    //connect to server
+    if(connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        std::cerr << "connection failed" << std::endl;
+        return 1;
+    }
+    
+    const char* message = "hello from server";
+    send(sock, message, strlen(message), 0);
+
+    //接收响应
+    char buffer[1024] = {0};
+    recv(sock, buffer, sizeof(buffer), 0);
+    std::cout << "server: "<<buffer <<std::endl;
+
+    close(sock);
+    return 0;
+}
